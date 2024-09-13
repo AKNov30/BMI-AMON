5dd49de5b594cf235c16bf1f7b603d4fb6d3b619
DESKTOP-I7GPUKI\Amon <blanc2666@gmail.com>
2024-09-13 09:04:30 +0700

    mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import Bmi from './pages/BMI'
-// import Bmi from './pages/Bmi';
-
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [Mode, setMode] = useState(false);
+
+  const changeMode = () => {
+    setMode(!Mode);
+
+    if (Mode) {
+      document.body.classList.remove('dark-mode');
+      document.body.classList.add('light-mode');
+    } else {
+      document.body.classList.remove('light-mode');
+      document.body.classList.add('dark-mode');
+    }
+  };
 
   return (
     <>
       <Bmi></Bmi>
+      <button onClick={changeMode}>{Mode ? 'Dark Mode' : 'Light Mode'}</button>
     </>
   )
 }
